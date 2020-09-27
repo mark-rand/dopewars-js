a766b8de52e88a253b0828298f9fb4f9b39be286
Mark Randall <mark@planetrandll.com>
2020-09-27 16:52:32 +0100

    got router working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,71 @@
 import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import DopewarsGame from "./DopewarsGame";
+import "./App.css";
 
 class App extends Component {
   render() {
     this.game = new DopewarsGame();
-    // return <DrugList game={this.game} />;
     return (
       <BrowserRouter>
-        {/* <Switch> */}
-          {/* <Route path='/' component={DrugList} /> */}
-          <DrugList game={this.game}/>
-        {/* </Switch> */}
+        {nav()}
+        <div>
+          <Switch>
+            <Route path="/about">Hello</Route>
+            <Route path="/">
+              <DrugList game={this.game} />
+            </Route>
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
 }
 
+function nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        Dopewars
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">
+              Home <span className="sr-only">(current)</span>
+              {/*need to sort this out*/}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/topics">
+              Topics
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
 class DrugList extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Dopewars</h1>
-        </header>
-        {this.props.game.getDrugTable()}
-      </div>
-    );
+    return <div className="App">{this.props.game.getDrugTable()}</div>;
   }
 }
 
--- src/DopewarsGame.js
+++ src/DopewarsGame.js
@@ -19,7 +19,13 @@ class Drug {
   }
 
   getFormattedData() {
-    return <tr><td>{this.name}</td><td>{this.quantity}</td><td>{this.price}</td></tr>
+    return (
+      <tr key={this.name}>
+        <td>{this.name}</td>
+        <td>{this.quantity}</td>
+        <td>{this.price}</td>
+      </tr>
+    );
   }
 
   static getHeader() {
@@ -37,24 +43,57 @@ class Drug {
 class DopewarsGame {
   constructor() {
     this.drugs = [
-      new Drug('Acid', 1000, 4400, true, false, 'The market is flooded with cheap home-made acid!'),
-      new Drug('Cocaine', 15000, 29000, false, true, ''),
-      new Drug('Hashish', 480, 1280, true, false,'The Marrakesh Express has arrived!'),
-      new Drug('Heroin', 5500, 13000, false, true, ''),
-      new Drug('Ludes', 11, 60, true, false, 'Rival drug dealers raided a pharmacy and are selling cheap ludes!'),
-      new Drug('MDA', 1500, 4400, false, false, ''),
-      new Drug('Opium', 540, 1250, false, true, ''),
-      new Drug('PCP', 1000, 2500, false, false, ''),
-      new Drug('Peyote', 220, 700, false, false, ''),
-      new Drug('Shrooms', 630, 1300, false, false, ''),
-      new Drug('Speed', 90, 250, false, true, ''),
-      new Drug('Weed', 315, 890, true, false, 'Columbian freighter dusted the Coast Guard! <br/>Weed prices have bottomed out!'),
+      new Drug(
+        "Acid",
+        1000,
+        4400,
+        true,
+        false,
+        "The market is flooded with cheap home-made acid!"
+      ),
+      new Drug("Cocaine", 15000, 29000, false, true, ""),
+      new Drug(
+        "Hashish",
+        480,
+        1280,
+        true,
+        false,
+        "The Marrakesh Express has arrived!"
+      ),
+      new Drug("Heroin", 5500, 13000, false, true, ""),
+      new Drug(
+        "Ludes",
+        11,
+        60,
+        true,
+        false,
+        "Rival drug dealers raided a pharmacy and are selling cheap ludes!"
+      ),
+      new Drug("MDA", 1500, 4400, false, false, ""),
+      new Drug("Opium", 540, 1250, false, true, ""),
+      new Drug("PCP", 1000, 2500, false, false, ""),
+      new Drug("Peyote", 220, 700, false, false, ""),
+      new Drug("Shrooms", 630, 1300, false, false, ""),
+      new Drug("Speed", 90, 250, false, true, ""),
+      new Drug(
+        "Weed",
+        315,
+        890,
+        true,
+        false,
+        "Columbian freighter dusted the Coast Guard! <br/>Weed prices have bottomed out!"
+      ),
     ];
   }
 
   getDrugTable() {
     const drugList = this.drugs.map((drug) => drug.getFormattedData());
-    const table = <table class='Drug-table'>{Drug.getHeader()}{drugList}</table>;
+    const table = (
+      <table className="Drug-table">
+        <thead>{Drug.getHeader()}</thead>
+        <tbody>{drugList}</tbody>
+      </table>
+    );
     return table;
   }
 }
